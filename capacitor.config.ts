9c542b2855394ee037aabca7c2cda2b0942d7891
nnivxix <adhe.hanasa@gmail.com>
2025-04-24 14:46:32 +0700

    feat: add Android build configuration and environment variables

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,26 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
+import { loadEnv } from "vite";
+
+const credentials = loadEnv("", process.cwd(), "");
+const KEY_STORE_PATH = credentials.KEY_STORE_PATH;
+const KEY_STORE_ALIAS = credentials.KEY_STORE_ALIAS;
+const KEY_STORE_PASSWORD = credentials.KEY_STORE_PASSWORD;
+const KEY_STORE_ALIAS_PASSWORD = credentials.KEY_STORE_ALIAS_PASSWORD;
 
 const config: CapacitorConfig = {
-  appId: 'com.notea.app',
-  appName: 'notea',
-  webDir: 'dist'
+  appId: "com.notea.app",
+  appName: "notea",
+  webDir: "dist",
+
+  android: {
+    buildOptions: {
+      releaseType: "APK",
+      keystorePath: KEY_STORE_PATH,
+      keystoreAlias: KEY_STORE_ALIAS,
+      keystorePassword: KEY_STORE_PASSWORD,
+      keystoreAliasPassword: KEY_STORE_ALIAS_PASSWORD,
+    },
+  },
 };
 
 export default config;
