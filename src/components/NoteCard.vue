3e393c72a0f88138ac48a09e249b51d2f8abf391
nnivxix <adhe.hanasa@gmail.com>
2025-04-27 19:32:52 +0700

    feat: update NoteCard layout for improved styling and content display

--- src/components/NoteCard.vue
+++ src/components/NoteCard.vue
@@ -28,17 +28,15 @@ const rowEnd = computed(() => {
       params: { uid: note.id },
     }"
     :key="note.id"
-    class="p-4 bg-white rounded-xl shadow"
-    :style="{
-      'grid-row-end': rowEnd,
-    }"
+    class="p-4 bg-white rounded-xl shadow col-span-2"
+    :class="[note.color ? note.color : 'bg-white']"
   >
     <time :datetime="note.updatedAt as string" class="text-gray-400 text-sm">{{
       formatTime(note.updatedAt as string)
     }}</time>
     <h2 class="text-xl font-bold line-clamp-1">{{ note.title }}</h2>
     <div
-      class="mt-2 max-h-72 overflow-hidden"
+      class="mt-2 max-h-32 overflow-hidden"
       v-html="note.content"
       ref="contentEl"
     />
